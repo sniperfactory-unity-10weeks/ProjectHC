ee23111bab66541eb9b84180c34b0cd7d57f943e
kimhyeok <kimmang940715@naver.com>
2023-10-11 18:48:45 +0900

    총알 발사 구현 n초에 1회

--- Assets/TestScene/Scripts/Bullet.cs
+++ Assets/TestScene/Scripts/Bullet.cs
@@ -1,18 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float _speed = 55f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 dir = Vector3.forward;
+        transform.position += dir * _speed * Time.deltaTime;
     }
 }
